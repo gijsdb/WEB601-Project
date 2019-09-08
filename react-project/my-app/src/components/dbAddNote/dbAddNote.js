0fadd4657afa3c5191547c7e2fb7bd3bad662561
gijsdb <gijsdeblauw@gmail.com>
2019-09-08 19:16:07 +1200

    added a text editor for the note page

--- react-project/my-app/src/components/dbAddNote/dbAddNote.js
+++ react-project/my-app/src/components/dbAddNote/dbAddNote.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './dbAddNote.css';
+import MyEditor from '../textEditor/textEditor'
 import { Link } from 'react-router-dom'
 
 
@@ -15,7 +16,7 @@ const DBAddNote = () => {
             <Link to="/dbreadbooks" className="btnBookNewNote">Back</Link>
         </header>
         <div className="textEditorContainer">
-            <h2>The text editor will eventually appear here</h2>
+            <MyEditor></MyEditor>
         </div>
     </div>
       )
---
+++ react-project/my-app/src/components/textEditor/textEditor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+
+export default class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  render() {
+    return (
+        <div>
+            <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+            <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        </div>
+    );
+  }
+}
+
