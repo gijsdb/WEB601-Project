c958af2c58c8ab9029fd089f4122eb2ef639d4d0
gijsdb <gijsdeblauw@gmail.com>
2019-09-12 11:52:20 +1200

    form input now converts to object yay

--- react-project/my-app/src/components/dbAddBook/dbAddBook.js
+++ react-project/my-app/src/components/dbAddBook/dbAddBook.js
@@ -3,26 +3,69 @@ import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
 import './dbAddBook.css';
 
+class Book {
+    constructor(title, author, date, file) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.file = file;
+        
+    }
+}
 
 export default class DBAddBook extends React.Component {
+    
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleSubmit(event) {
+        event.preventDefault();
+        const lcTitle = document.getElementById('title').value;
+        const lcAuthor = document.getElementById('author').value;
+        const lcDate = document.getElementById('date').value;
+        const lcFile = document.getElementById('file').value;
+        var testBook = new Book(lcTitle, lcAuthor, lcDate, lcFile)
+        console.log(testBook);
+        //console.log(lcTitle + lcAuthor + lcDate + lcFile)
+      }
+
+
     render() {
 
-       
+        /*
         class Book {
             constructor(title, author, date, file) {
                 this.title = title;
                 this.author = author;
                 this.date = date;
                 this.file = file;
+                
             }
           
         }
-
-        function SubmitBook() {
-            var testBook = new Book(document.getElementsByName("title").values,document.getElementsByName("author").values,document.getElementsByName("date").values,document.getElementsByName("file").values)
-            return testBook;
+        const CreateBook = () => {
+            function useThisInfo(e) {
+                e.preventDefault();
+                const lcTitle = document.getElementsByName('title').value;
+                const lcAuthor = document.getElementsByName('author').value;
+                var testBook = new Book(lcTitle,lcAuthor);
+                return testBook
+            }
         }
+        const SubmitBook = (event) => {
+            var lcTitle = document.getElementsByName('title')
+            var lcAuthor = document.getElementsByName('author')
+            var lcDate = document.getElementsByName('date')
+            var lcFile = document.getElementsByName('file')
+            var testBook = new Book(lcTitle,lcAuthor,lcDate,lcFile)
+            console.log("I have fired")
+            return testBook;
+        }*/
+
     
+
         return(
             <div className="addBookContainer">
                 <header>
@@ -30,24 +73,24 @@ export default class DBAddBook extends React.Component {
                     <Link to="/dbreadbooks" className="btnBookNewNote">Back</Link>
                 </header>
                 <div className='bookInput'>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                     <label>
                         Title:
-                        <input type="text" name="title"/>
+                        <input type="text" id="title"/>
                     </label><br></br>
                     <label>
                         Author:
-                        <input type="text" name="author"/>
+                        <input type="text" id="author"/>
                     </label><br></br>
                     <label>
                         Date read:
-                        <input type="date" name="date"/>
+                        <input type="date" id="date"/>
                     </label><br></br>
                     <label>
                         Book cover:
-                        <input type="file" name="file" />
+                        <input type="file" id="file" />
                     </label><br></br>
-                    <input type="submit" value="Submit" onSubmit={SubmitBook()}/>
+                    <input type="submit" id="Submit"/>
                     </form>
                 </div>
             </div>
