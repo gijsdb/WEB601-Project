ca4013fe8642d261d183ac3613821024e9eb65f8
gijsdb <gijsdeblauw@gmail.com>
2019-10-04 16:44:09 +1300

    now retrieves books stored in SQL

--- react-project/my-app/src/components/dbBookTile/dbBookTile.js
+++ react-project/my-app/src/components/dbBookTile/dbBookTile.js
@@ -3,16 +3,36 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import './dbBookTile.css';
 
-export default class DBBookTile extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
 
-    render() {
+    const DBBookTile = ({ books }) => {
+        return (
+          <div>
+            {books.map((book) => (
+                <div class="module">
+                  <h3>{book.Title}</h3><br/>
+                  <h3>{book.Author}</h3><br/>
+                  <h3>{book.DateRead}</h3><br/>
+                </div>
+            ))}
+          </div>
+        )
+      };
+      export default DBBookTile
+
+
+
+
+    /*render() {
         return (
             <div>
-                <div className="module">Book test</div>
+                    {books.map((book) => (
+                    <div class="module">
+                        <h1>{book.Title}</h1>
+                        <h1>{book.Author}</h1>
+                        <h1>{book.DateRead}</h1>
+                     </div>
+                ))}
             </div>
         )
-    }
-}
+    }*/
+
--- react-project/my-app/src/components/dbReadBooks/dbReadBooks.js
+++ react-project/my-app/src/components/dbReadBooks/dbReadBooks.js
@@ -4,24 +4,50 @@ import './dbReadBooks.css';
 import { Link } from 'react-router-dom'
 import DBBookTile from '../dbBookTile/dbBookTile'
 
-const myBooks = [];
+//const myBooks = [];
 export default class DBReadBooks extends React.Component {
    
-    constructor() {
-        super();
-        this.state = myBooks
+    constructor(props) {
+		super(props);
+		   this.state = ({
+		      isFetching: false,
+              books: [],
+		   })
+	}
+
+    GetUserBooks() {
+        fetch('http://localhost:4200/api/books')
+		.then(res => res.json())
+		.then(data => {
+			if(data.cod === '404') {
+				this.setState({
+					isFetching: false,
+				})
+			} else {
+                this.setState({
+                isFetching: true,
+                books: data, 
+            });
+            }
+		})
+		.catch(err => {
+		   console.log(err);
+        })	
     }
+
+    componentDidMount() {
+        this.GetUserBooks();
+        //this.timer = setInterval(() => this.GetUserBooks(), 10000);
+        //setTimeout(function() {console.log(this.state.books)}, 8000)
+	}
 
 
     render() {
-        
         return(
+           
             <div className="readContainer">
             <div className="readBookGrid">
-                 <DBBookTile/>      
-                 <DBBookTile/>   
-                 <DBBookTile/>   
-                 <DBBookTile/>          
+                 <DBBookTile books={this.state.books}/>              
                  <div className="module" ><Link to="/dbaddbook" className="navLink">Add book</Link></div>
              </div>
          </div>
