69b83e423e6548d64f8aeb23a1ff909f9f8e29e6
gijsdb <gijsdeblauw@gmail.com>
2019-11-20 14:07:08 +1300

    SQL for project

--- database.sql
+++ database.sql
@@ -3,17 +3,17 @@ CREATE DATABASE web601_project;
 
 use web601_project;
 
-
+-- All the create table statements
 DROP TABLE IF EXISTS tblUsers;
 CREATE TABLE tblUsers (
-	ID int(10) auto_increment PRIMARY KEY,
+	ID int auto_increment PRIMARY KEY,
 	Email varchar(50),
     `Password` varchar(50)
 );
 
 DROP TABLE IF EXISTS tblBooks;
 CREATE TABLE tblBooks (
-	ID int(10) auto_increment PRIMARY KEY,
+	ID int auto_increment PRIMARY KEY,
 	Title varchar(50),
 	Author varchar(50),
     DateRead date,
@@ -23,21 +23,20 @@ CREATE TABLE tblBooks (
 
 DROP TABLE IF EXISTS tblNotes;
 CREATE TABLE tblNotes (
-	ID int(10) auto_increment PRIMARY KEY,
+	ID int auto_increment PRIMARY KEY,
 	Title varchar(50),
 	Content varchar(1000),
     BookID int,
     FOREIGN KEY (BookID) REFERENCES tblBooks(ID)
 );
 
+-- Initial test data
 INSERT INTO tblUsers(Email, `Password`)
 VALUES('test@test.com', 'test123');
 SELECT * FROM tblUsers;
 
-INSERT INTO tblBooks(Title, Author, DateRead, UserID)
-VALUES('Test Book', 'Test author', '1997-6-04',1);
 SELECT * FROM tblBooks;
 
 INSERT INTO tblNotes(Title, Content, BookID)
-VALUES('Test Note', 'I like this book', 1);
+VALUES('Test Note', 'I like this book', 1),('Test Note2', 'I like this book2', 1),('Test Note3', 'I like this book2', 1),('Test Note4', 'I like this book2', 3);
 SELECT * FROM tblNotes;
